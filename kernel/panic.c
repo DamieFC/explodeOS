056ed92d33516080f65d3901207f31dcf38a6082
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-07 16:30:16 -0700

    Added logging and beeping to kernel panic

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <kernel/log/log.h>
  
 __attribute__((__noreturn__))
 void panic(void) {
 #if defined(__is_libk)
-	// TODO: Add proper kernel panic.
-	printf("kernel: panic: panic()\n");
+	/* TODO: Add proper kernel panic. */
+	printf("kernel: panic: panic():\n");
+	printf("PANIC!!!!!!!!\n\a\a\a"); /* Hopefully it beeps */
+	printf("The panic happened with __is_libk defined.");
+	log(PANIC, "Kernel panic happened! Yikes! Happened with __is_libk defined.");
 #else
-	// TODO: Abnormally terminate the process as if by SIGABRT.
-	printf("panic()\n");
+	/* TODO: Abnormally terminate the process as if by SIGABRT. */
+	printf("PANIC!!!!!!!!\n\a\a\a"); /* Hopefully it beeps. */
+	printf("The panic happened without __is_libk defined.");
+	log(PANIC, "Kernel panic happened! Yikes! Happened without __is_libk defined.");
 #endif
 	while (1) { }
 	__builtin_unreachable();
+	printf("\a")
 }
