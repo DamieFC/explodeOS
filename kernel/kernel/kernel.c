229a8b54e775e48a2e81ed60da383fb3951a9bd5
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-07 16:19:55 -0700

    Update kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -8,5 +8,5 @@ void kernel_main(void) {
     printf("Welcome to explodeOS!\n");
     printf("This is from OSDev.org with modifications from DamieFC on Github!\n");
     printf("If you want to help, go to https://github.com/DamieFC/explodeOS and check it out!\n");
-    printf("Hopefully you didn't install this on your computer, for two reasons: 1, you can't do anything, and 2, you can go to a browser window and check out the code!\n");
+    printf("Hopefully you didn't install this on your computer, for two reasons: 1, you can't do anything, and 2, you can go to a browser window and check out the code, and contribute so it\'s actually useful!\n");
 };
