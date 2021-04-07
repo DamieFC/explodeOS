5b5e53cb0391827ebda07d228e12d1f13a7c2416
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-07 14:00:02 -0700

    Please help me with this stuff

---
+++ kernel/devices/keyboard/kemap.h
@@ -0,0 +1,9 @@
+/**************************************************************
+ * explodeOS keymap things for keyboard                       *
+ * (c) DamieFC 2021 under the MIT license                     *
+ **************************************************************/
+#include <stdio.h>
+
+/*char keymap[104] = {
+   Help me pls 
+}*/
