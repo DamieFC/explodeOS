cd81cfec5786266e475c690cacf94799e5c37f6d
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-07 19:25:18 -0700

    Changing it to keyboard.h and starting with byte layout thing

---
+++ kernel/devices/keyboard/keyboard.h
@@ -0,0 +1,12 @@
+/**************************************************************
+ * explodeOS keykeyboard things for keyboard                  *
+ * (c) DamieFC 2021 under the MIT license                     *
+ **************************************************************/
+#include <stdio.h>
+
+char keymap[6 /* So far */] = {
+    /* Help me pls I don't really get bytes */
+    0x01,0x02,0x03,0x04,0x05, /* OK literally esc 1 2 3 4 */
+}
+
+void keyboard_init();
--- kernel/devices/keyboard/keymap.h
+++
@@ -1,9 +0,0 @@
-/**************************************************************
- * explodeOS keymap things for keyboard                       *
- * (c) DamieFC 2021 under the MIT license                     *
- **************************************************************/
-#include <stdio.h>
-
-/*char keymap[104] = {
-   Help me pls 
-}*/
