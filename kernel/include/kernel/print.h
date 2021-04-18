46dee7d1003dca19929b382036c977b7ff80c6fd
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-18 12:47:47 -0700

    Text printing stuff

---
+++ kernel/include/kernel/print.h
@@ -0,0 +1,10 @@
+/**********************************************************************************
+ * My code for printing stuff on screen, adapted from terminal_writestring() and  *
+ * other functions in `tty.h`, going to switch over code.                         *
+ * (c) 2021 DamieFC                                                               *
+ **********************************************************************************/
+
+#include <stddef.h>
+
+void print(const char* data); // print("Hi%c", exclamation);
+void printc(char c);
