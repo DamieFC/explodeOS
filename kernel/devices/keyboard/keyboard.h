cdf62d4a78c3c9ca6ddcc2e59ee08ed805240c8a
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-19 09:31:20 -0700

    Whoops forgot to change the number
    Ah, yes. The 14 key keyboard. Bruh.

--- kernel/devices/keyboard/keyboard.h
+++ kernel/devices/keyboard/keyboard.h
@@ -4,7 +4,7 @@
  **************************************************************/
 #include <stdio.h>
 
-char keymap[6 /* So far */] = {
+char keymap[14 /* So far */] = {
     /* Help me pls I don't really get bytes */
     0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E /* OK literally esc 1 2 3 4 5 6 7 8 9 0 - = backspace */
 }
