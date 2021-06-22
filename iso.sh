38275d432777fb6a52a4e9865eca34e650722f68
Adam Salhi <adam06genius@gmail.com>
2021-06-22 19:53:31 +0100

    Colors in GRUB menu, new utilities for terminal

--- iso.sh
+++ iso.sh
@@ -8,8 +8,19 @@ mkdir -p isodir/boot/grub
  
 cp sysroot/boot/explodeos.kernel isodir/boot/explodeos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "explodeos" {
+set timeout=60
+set default=0
+set menu_color_normal=blue/black
+set menu_color_highlight=white/blue
+
+menuentry "explodeOS" {
 	multiboot /boot/explodeos.kernel
+}
+menuentry "Reboot" {
+    reboot
+}
+menuentry "Shut Down" {
+	halt
 }
 EOF
 grub-mkrescue -o explodeos.iso isodir
